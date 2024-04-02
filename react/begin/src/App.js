2fb6d3727fdbd2ee040c7cf709e402e277a86f95
LAPTOP-RC7BFFPH\HP <zelaf44923@gmail.com>
2024-04-02 16:19:37 +0300

    add: managed and unmanaged input

--- react/begin/src/App.js
+++ react/begin/src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Counter from "./components/Counter"
 import ClassCounter from './components/ClassCounter';
 import PostItem from './components/PostItem';
 import PostList from './components/PostList';
 import './styles/App.css';
 import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 
 function App() {
   // let likes = 0;
@@ -23,13 +24,33 @@ function App() {
     {id: 2, title: 'Javascript 2', body: 'Description'},
     {id: 3, title: 'Javascript 3', body: 'Description'},
   ])
+// управляемый инпут (получить доступ из инпута)
+  const [title, setTitle] = useState('')
+// получаем доступ к DOM элементу
+  const bodyInputRef = useRef();
+
+  const addNewPost = (e) => {
+    e.preventDefault()
+    console.log(title)
+    console.log(bodyInputRef.current.value)
+  }
+
   //для каждого поста в массиве отрисовываем PostItem, как пропс передаём объект
   return (
     <div className="App">
       <form>
-        <input type="text" placeholder="Название поста"/>
-        <input type="text" placeholder="Описание поста"/>
-        <MyButton disabled>Создать пост</MyButton>
+        {/*Управляемый инпут*/}
+        <MyInput
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          type="text"
+          placeholder="Описание поста"/>
+        {/*Неуправляемый инпут*/}
+        <MyInput
+          ref={bodyInputRef}
+          type="text"
+          placeholder="Описание поста"/>
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
       </form>
       <PostList posts={posts} title="Посты про JS"/>
     </div>
